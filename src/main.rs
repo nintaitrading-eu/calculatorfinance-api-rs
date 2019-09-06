12def5f066935f8c4fca062832f4caaee00b2879
Andy Nagels <thereisanewway@gmail.com>
2019-09-06 11:46:31 +0200

    Hello world example with actix-web.

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate actix_web;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+fn index() -> impl Responder
+{
+  HttpResponse::Ok().body("Hello world!")
+}
+
+fn main()
+{
+  HttpServer::new(||
+  {
+    App::new()
+      .route("/", web::get().to(index))
+  })
+  .bind("127.0.0.1:8888")
+  .unwrap()
+  .run()
+  .unwrap();
+}

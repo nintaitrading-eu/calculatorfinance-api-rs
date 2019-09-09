4924ea1f7c4115b8d25119fd4010a118314bf697
Andy Nagels <a.nagels@farmad.be>
2019-09-09 10:10:59 +0200

    Added the general functions.

--- src/api_1/general.rs
+++ src/api_1/general.rs
@@ -2,7 +2,17 @@ extern crate libcalculatorfinance;
 
 use actix_web::{web, HttpResponse};
 
+pub fn calculate_percentage_of(params: web::Path<(f64, f64)>) -> HttpResponse
+{
+    HttpResponse::Ok().json(libcalculatorfinance::calculate_percentage_of(params.0, params.1))
+}
+
 pub fn convert_from_orig(params: web::Path<(f64, f64)>) -> HttpResponse
 {
     HttpResponse::Ok().json(libcalculatorfinance::convert_from_orig(params.0, params.1))
+}
+
+pub fn convert_to_orig(params: web::Path<(f64, f64)>) -> HttpResponse
+{
+    HttpResponse::Ok().json(libcalculatorfinance::convert_to_orig(params.0, params.1))
 }
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,23 @@ fn main()
     HttpServer::new(|| {
         App::new()
             .service(web::resource("/status").route(web::get().to(api_1::status::status)))
+
+            /* General */
             .service(
-                web::resource("/convert_from_orig/{value}/{from_value}")
-                    .route(web::get().to(api_1::general::convert_from_orig)),
-            )
+                web::resource("/calculate_percentage_of/{value}/{from_value}")
+                    .route(web::get().to(api_1::general::calculate_percentage_of)))
+            .service(
+                web::resource("/convert_from_orig/{price}/{exchange_rate}")
+                    .route(web::get().to(api_1::general::convert_from_orig)))
+            .service(
+                web::resource("/convert_to_orig/{converted_price}/{exchange_rate}")
+                    .route(web::get().to(api_1::general::convert_to_orig)))
+
+            /* Before trade */
+            // TBD
+
+            /* After trade */
+            // TBD
     })
     .bind("127.0.0.1:8888")
     .unwrap()
